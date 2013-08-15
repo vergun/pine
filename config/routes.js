b9ce2dc15ea009ab6f979e0bf39f09cda376952d
bsorem-sugarcrm <bsorem@sugarcrm.com>
2013-08-15 10:54:01 -0700

    upgrade sails to v0.9.3

--- config/routes.js
+++ config/routes.js
@@ -1,38 +1,14 @@
-// Routes
-// *********************
-// 
-// This table routes urls to controllers/actions.
-//
-// If the URL is not specified here, the default route for a URL is:  /:controller/:action/:id
-// where :controller, :action, and the :id request parameter are derived from the url
-//
-// If :action is not specified, Sails will redirect to the appropriate action 
-// based on the HTTP verb: (using REST/Backbone conventions)
-//
-//		GET:	/:controller/read/:id
-//		POST:	/:controller/create
-//		PUT:	/:controller/update/:id
-//		DELETE:	/:controller/destroy/:id
-//
-// If the requested controller/action doesn't exist:
-//   - if a view exists ( /views/:controller/:action.ejs ), Sails will render that view
-//   - if no view exists, but a model exists, Sails will automatically generate a 
-//       JSON API for the model which matches :controller.
-//   - if no view OR model exists, Sails will respond with a 404.
-//
 module.exports.routes = {
-	
-	// To route the home page to the "index" action of the "home" controller:
-	'/' : {
-		controller: 'main',
-		action: 'index'
-	},
-	'/open' : {
-		controller: 'main',
-		action: 'open'
-	},
-	'/save' : {
-		controller: 'main',
-		action: 'save'
-	}
+  '/': {
+    controller: 'main',
+    action: 'index'
+  },
+  '/open': {
+    controller: 'main',
+    action: 'open'
+  },
+  '/save': {
+    controller: 'main',
+    action: 'save'
+  }
 };
