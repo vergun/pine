87bb54e13bf9c4774a345e5835cd8ee613825b16
Verdi Ergun <vergun@gmail.com>
2013-11-10 17:30:40 -0800

    Animations/effects for clicks

--- assets/linker/js/index.js
+++ assets/linker/js/index.js
@@ -1,6 +1,30 @@
-function ArticleController($scope) {  
-  $scope.click = function() {
-    $scope.hide();
-  };
+// angular.module('article', [])
+// .controller('ArticleCtrl', ['$scope', function($scope) {
+//   
+//   $scope.click = function($event) {
+//   };
+//   
+// }]);
 
+/// jquery implementation
+$(document).on('click', 'ul.article-list li.folder', function() {
+  runClickedFolder(this);
+})
+
+var runClickedFolder = function(el) {
+  slideToggle(el);
+  swapExpandCollapseIcon(el);
+}
+
+var slideToggle = function(el) {
+  $(el).parent().children('ul').toggle('fast');
+}
+
+var swapExpandCollapseIcon = function(el) {
+  element = $(el).find('i.switch')
+  if (element.hasClass('icon-collapse-alt switch')) {
+    element.attr('class', 'icon-expand-alt switch')
+  } else {
+    element.attr('class', 'icon-collapse-alt switch')
+  }
 }
