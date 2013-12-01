70df998be98f4b18d8cc2d4e4379dc89d658158b
Verdi Ergun <vergun@gmail.com>
2013-11-30 19:52:16 -0800

    Fix for source code modal

--- assets/linker/js/article/show.js
+++ assets/linker/js/article/show.js
@@ -1,4 +1,4 @@
 /// Fix for Bootstrap Modals not affecting dom layout but rendering underneath elemetns unclickable
-$(document).on('click', 'a[data-target="#myModal"]', function() {
+$(document).on('click', 'a[data-target="#myModal"], #myModal button.close, #myModal button[data-dismiss="modal"]', function() {
   $('#myModal').toggleClass('z-index-fix');
 });
