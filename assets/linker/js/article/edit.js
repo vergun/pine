7c9e2a2981331bb9f8451dc9223a1a059b632ee3
Verdi Ergun <vergun@gmail.com>
2013-12-01 16:25:54 -0800

    Fix for save article form, submit params

---
+++ assets/linker/js/article/edit.js
@@ -0,0 +1,8 @@
+jQuery(function() {
+  
+  // Submit form when clicking save link
+  jQuery(document).on('click', '#edit-article-save', function() {
+    jQuery('#save-article-form').submit();
+  })
+  
+});
