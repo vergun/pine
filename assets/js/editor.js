4ae7192f5959aec990b2aa34d5a628707ceea9b0
Verdi Ergun <vergun@gmail.com>
2014-02-16 13:14:44 -0800

    Removed hallo and rangy from the project

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,38 +1,39 @@
-// Modified from http://hallojs.org/demo/markdown/
+tinymce.init({
+    selector: "div.editable",
+    theme: "modern",
+    plugins: [
+        ["advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker"],
+        ["searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking"],
+        ["save table contextmenu directionality emoticons template paste style"]
+    ],
+    add_unload_trigger: false,
+    schema: "html5",
+    inline: true,
+    toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image     | print preview media",
+    statusbar: false
+});
 
+// Todo implemenet:
+// Update HTML every time content is modified
 jQuery(document)
     .ready(function() {
-    // Enable Hallo editor
-    jQuery('.editable')
-        .hallo({
-        plugins: {
-            'halloformat': {},
-            'halloheadings': {},
-            'hallojustify': {},
-            'hallolists': {},
-            'halloreundo': {},
-            'halloimage': {}
-        },
-        toolbar: 'halloToolbarFixed'
-    });
 
-    // Method that converts the HTML contents to Markdown
-    var showSource = function(content) {
-        jQuery('#source')
-            .get(0)
-            .value = content;
-    };
+var showSource = function(content) {
+    jQuery('#source')
+        .get(0)
+        .value = content;
+};
 
-    // Update HTML every time content is modified
-    jQuery('.editable')
-        .bind('hallomodified', function(event, data) {
-        showSource(data.content);
-    });
-    
-    var html = 
-      jQuery('.editable').length > 0 ? 
-      jQuery('.editable').html() : 
-      jQuery('.not-editable').html();
-      
-    showSource(html);
+jQuery('.editable')
+    .bind('hallomodified', function(event, data) {
+    showSource(data.content);
+});
+
+var html = 
+  jQuery('.editable').length > 0 ? 
+  jQuery('.editable').html() : 
+  jQuery('.not-editable').html();
+  
+showSource(html);
+
 });
