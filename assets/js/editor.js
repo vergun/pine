066e0fcc0d28d51e4a46d012eebb0d9b16668483
Verdi Ergun <vergun@gmail.com>
2014-02-21 15:08:23 -0800

    Remove style plugin, deprecated in tinymce 3.4 and not available in 4.x

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -4,7 +4,7 @@ tinymce.init({
     plugins: [
         ["advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker"],
         ["searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking"],
-        ["save table contextmenu directionality emoticons template paste style"]
+        ["save table contextmenu directionality emoticons template paste"]
     ],
     add_unload_trigger: false,
     schema: "html5",
